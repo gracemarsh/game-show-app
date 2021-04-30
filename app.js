23753ba913b91aaa96fe19ecee1797b2004b9d75
Grace <gracemarsh27@gmail.com>
2021-04-30 16:46:41 +1000

    completed first 3 steps

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
-console.log("hi there");
-
-const qwerty = document.getElementById("qwerty");
-const resetBtn = document.getElementById("btn__reset");
-let missed = 0;
-
+//try to keep variables in order that they appear in html
+const overlay = document.getElementById("overlay");
+const resetBtn = document.getElementsByClassName("btn__reset")[0];
 const phrase = document.getElementById("phrase");
-let phrases = [
-  "Some of the best words of art come from the most bruised and battered of hearts",
-  "Nostalgia is a liar that insists things were better than they seemed",
-  "I pour my heart out onto paper where it is safe",
-  "If love is a disease then I am very sick but incredibly happy",
-  "I am the moon I too have my phases",
+const phrases = [
+  "A blessing in disguise",
+  "Better late than never	",
+  "Your guess is as good as mine",
+  "The best of both worlds",
+  "Like the moon I too have my phases",
 ];
+const qwerty = document.getElementById("qwerty");
+let missed = 0;
 
 // reset game
 resetBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+  // e.preventDefault();
+  resetBtn.style.display = "none";
+  overlay.style.display = "none";
 });
 
-//return a random phrase form an array
-const getRandomPhraseAsArray = (arr) => {};
+//return a random phrase from an array
+const getRandomPhraseAsArray = (arr) => {
+  const randomIndex = Math.floor(Math.random() * phrases.length);
+  const randomPhrase = arr[randomIndex];
+  return randomPhrase;
+};
+const randomPhrase = getRandomPhraseAsArray(phrases);
 
 //adds the letters of a string to the display
-const addPhraseToDisplay = (arr) => {};
+
+const addPhraseToDisplay = (arr) => {
+  let phraseLetters = randomPhrase.split("");
+  let ul = document.querySelector("#phrase ul");
+  phraseLetters.forEach((letter) => {
+    console.log(letter);
+    let newLi = document.createElement("li");
+    newLi.textContent = letter;
+    ul.appendChild(newLi);
+
+    if (letter === " ") {
+      newLi.classList.add("space");
+    } else {
+      newLi.classList.add("letter");
+    }
+  });
+};
+addPhraseToDisplay();
+
+/*
+
+
+
+
+
 
 // check if a letter is in the phrase
 const checkLetter = (button) => {};
@@ -32,3 +62,4 @@ startButton.addEventListener("click", () => {});
 
 //listen for the onscreen ekyboard to be clicked
 qwerty.addEventListener("click", (e) => {});
+*/
